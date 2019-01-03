ecb52fa9a1f422da7ff7d48143169814e12b6409
Sandro Petrillo Burri <gibo.gaf@gmail.com>
2019-01-03 09:50:06 +0000

    version 0.0.5

--- R/distrr.R
+++ R/distrr.R
@@ -1,21 +1,25 @@
-#' Tools to estimate and manage empirical distributions
+#' Estimate and manage empirical distributions
 #' 
 #' Tools to estimate and manage empirical distributions,
 #' which should work with survey data. One of the main features is the 
 #' possibility to create data cubes of estimated statistics, that include
 #' all the combinations of the variables of interest (see for example functions
-#' 'dcc' and 'dcc2').
+#' dcc5() and dcc6()).
 #' 
 #' @docType package
 #' @name distrr
 #' 
-#' @import lazyeval
-#' @importFrom dplyr group_by_ summarise_ mutate_ filter_ arrange_ select_ mutate_all bind_cols bind_rows combine ungroup
+#' @import rlang
+#' @import dplyr
 #' @importFrom utils combn
 #' @importFrom stats na.omit setNames quantile
 #' @importFrom tidyr expand expand_ complete_
 NULL
 
+# Sostituisco
+# #' @importFrom dplyr group_by_ summarise_ mutate_ filter_ arrange_ select_ mutate_all bind_cols bind_rows combine ungroup n
+# con (per il momento)
+# #' @import dplyr
 
 
 #' @importFrom magrittr %>%
